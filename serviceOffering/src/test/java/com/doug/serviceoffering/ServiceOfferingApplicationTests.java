159a1f68a17916316c5232a640d794c35d2e4589
douglanyabasa <douglasnyabasa400@gmail.com>
2026-03-13 14:30:48 +0200

    added 4 modules to work on

---
+++ serviceOffering/src/test/java/com/doug/serviceoffering/ServiceOfferingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doug.serviceoffering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOfferingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
