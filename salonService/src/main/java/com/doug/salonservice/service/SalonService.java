9fd33045dc00d2f0f52e9ad53674898166677b94
douglanyabasa <douglasnyabasa400@gmail.com>
2026-03-15 08:24:20 +0200

    added salon service

--- salonService/src/main/java/com/doug/salonservice/service/SalonService.java
+++ salonService/src/main/java/com/doug/salonservice/service/SalonService.java
@@ -2,8 +2,19 @@ package com.doug.salonservice.service;
 
 import com.doug.salonservice.models.Salon;
 import com.doug.salonservice.payload.dto.SalonDT0;
+import com.doug.salonservice.payload.dto.UserDTO;
+
+import java.util.List;
 
 public interface SalonService {
 
     Salon createSalon(SalonDT0 salon, UserDTO user);
+
+    Salon updateSalon(SalonDT0 salon, UserDTO user,Long salonId);
+
+    List<Salon> getAllSalons();
+    Salon getSalonById(Long salonId);
+    Salon getSalonByOwnerId(Long ownerId);
+    List<Salon> searchSalonByCityName(String city);
+
 }
