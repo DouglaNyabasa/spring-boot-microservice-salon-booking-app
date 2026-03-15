7d867d3b079d1f5999b6da26aed598ee1490ea26
douglanyabasa <douglasnyabasa400@gmail.com>
2026-03-15 19:05:05 +0200

    added get service by salon id method in controller

---
+++ serviceOffering/src/main/java/com/doug/serviceoffering/controller/ServiceOfferingController.java
@@ -0,0 +1,26 @@
+package com.doug.serviceoffering.controller;
+
+import com.doug.serviceoffering.models.ServiceOffering;
+import com.doug.serviceoffering.service.ServiceOfferingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/serviceOffering")
+public class ServiceOfferingController {
+
+
+    private final ServiceOfferingService serviceOfferingService;
+
+    public ServiceOfferingController(ServiceOfferingService serviceOfferingService) {
+        this.serviceOfferingService = serviceOfferingService;
+    }
+
+    @GetMapping("/salon/{salonId}")
+    public ResponseEntity<Set<ServiceOffering>> getServiceOfferingBySalonId(@PathVariable Long salonId, @RequestParam(required = false) Long categoryId){
+      Set<ServiceOffering> serviceOfferings =  serviceOfferingService.getAllServiceBySalon(salonId,categoryId);
+      return ResponseEntity.ok(serviceOfferings);
+    }
+}
