d82ed0e0138a98d798d39fcb48e6fbecb9ac2375
douglanyabasa <douglasnyabasa400@gmail.com>
2026-03-15 16:08:09 +0200

    added get category by salon in controller

--- categoryService/src/main/java/com/doug/categoryservice/controller/Controller.java
+++ categoryService/src/main/java/com/doug/categoryservice/controller/Controller.java
@@ -1,9 +1,29 @@
 package com.doug.categoryservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.doug.categoryservice.models.Category;
+import com.doug.categoryservice.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/categories")
 public class Controller {
+
+    private final CategoryService categoryService;
+
+    public Controller(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+
+    @PostMapping("/create")
+    @GetMapping("/salon/{id}")
+    public ResponseEntity<Set<Category>> getCategoriesBySalon(@PathVariable Long id){
+        Set<Category> categories = categoryService.getAllCategoriesBySalon(id);
+
+        return ResponseEntity.ok(categories);
+    }
+
 }
