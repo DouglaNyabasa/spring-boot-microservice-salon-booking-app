a7b6fd7d4ee028ede4a43ece906f314bfdfe4316
douglanyabasa <douglasnyabasa400@gmail.com>
2026-03-19 19:54:07 +0200

    added create stripe payment link method

--- paymentService/src/main/java/com/doug/paymentservice/service/PaymentService.java
+++ paymentService/src/main/java/com/doug/paymentservice/service/PaymentService.java
@@ -5,13 +5,14 @@ import com.doug.paymentservice.model.PaymentOrder;
 import com.doug.paymentservice.payload.BookingDTO;
 import com.doug.paymentservice.payload.Response.PaymentLinkResponse;
 import com.doug.paymentservice.payload.UserDTO;
+import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentLink;
 
 public interface PaymentService {
 
- PaymentLinkResponse createOrder(UserDTO userDT0, BookingDTO bookingDTO, PaymentMethod paymentMethod);
+ PaymentLinkResponse createOrder(UserDTO userDT0, BookingDTO bookingDTO, PaymentMethod paymentMethod) throws StripeException;
  PaymentOrder getPaymentOrderById(Long id) throws Exception;
  PaymentOrder getPaymentOrderByPaymentId(String paymentId);
- String  createStripePaymentLink(UserDTO userDTO,Long amount, Long orderId );
+ String  createStripePaymentLink(UserDTO userDTO,Long amount, Long orderId ) throws StripeException;
 
 }
--- paymentService/src/main/java/com/doug/paymentservice/service/impl/PaymentServiceImpl.java
+++ paymentService/src/main/java/com/doug/paymentservice/service/impl/PaymentServiceImpl.java
@@ -7,6 +7,10 @@ import com.doug.paymentservice.payload.Response.PaymentLinkResponse;
 import com.doug.paymentservice.payload.UserDTO;
 import com.doug.paymentservice.repository.PaymentOrderRepository;
 import com.doug.paymentservice.service.PaymentService;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +28,7 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
-    public PaymentLinkResponse createOrder(UserDTO userDT0, BookingDTO bookingDTO, PaymentMethod paymentMethod) {
+    public PaymentLinkResponse createOrder(UserDTO userDT0, BookingDTO bookingDTO, PaymentMethod paymentMethod) throws StripeException {
         Long amount = (long)bookingDTO.getTotalPrice();
 
 
@@ -55,7 +59,30 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
-    public String createStripePaymentLink(UserDTO userDTO, Long amount, Long orderId) {
-        return "";
+    public String createStripePaymentLink(UserDTO userDTO, Long amount, Long orderId) throws StripeException {
+        Stripe.apiKey=stripeSecretKey;
+        SessionCreateParams params = SessionCreateParams.builder()
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("http://localhost:3000/payment-success/" + orderId)
+                .setCancelUrl("http://localhost:3000/payment-cancel")
+                .addLineItem(SessionCreateParams.LineItem.builder()
+                        .setQuantity(1L)
+                        .setPriceData(SessionCreateParams.LineItem.PriceData.builder()
+                                .setCurrency("usd")
+                                .setUnitAmount(amount * 100)
+                                .setProductData(SessionCreateParams
+                                        .LineItem
+                                        .PriceData
+                                        .ProductData
+                                        .builder()
+                                        .setName("salon appointment booking")
+                                        .build()
+                                ).build()
+                        ).build()
+                ).build();
+
+        Session session = Session.create(params);
+        return session.getUrl();
     }
 }
